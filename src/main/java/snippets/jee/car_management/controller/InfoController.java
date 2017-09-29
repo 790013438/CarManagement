35c2c542cc716d0320a68cb46853e93cbb6051a4
790013438 <790013438@qq.com>
2017-09-29 10:50:22 +0800

    controller info maven build

---
+++ src/main/java/snippets/jee/car_management/controller/InfoController.java
@@ -0,0 +1,21 @@
+package snippets.jee.car_management.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import snippets.jee.car_management.dao.InfoDAO;
+
+@Controller
+public class InfoController {
+
+    @Autowired
+    InfoDAO infoDAO;
+
+    @RequestMapping("/infos")
+    public String getCourses (Model model) {
+        model.addAttribute("infos", infoDAO.getInfos());
+        return "infos";
+    }
+}
